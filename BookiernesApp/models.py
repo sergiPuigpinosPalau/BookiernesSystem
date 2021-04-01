d218055e130bfb858c5c213666a2e4116003f9fd
Sergi Puigpinos Palau <sergi.puigpinos@hotmail.com>
2021-04-01 22:53:47 +0200

    Updated models

--- BookiernesApp/models.py
+++ BookiernesApp/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 
-
+#TODO anyadir foto perfil
 class User(AbstractUser):
     USER_TYPE_CHOICES = [('writer', 'Writer'), ('editor', 'Editor'), ('main_editor', 'Main editor'), ]
     user_type = models.CharField(
-        max_length=20,
+        max_length=255,
         choices=USER_TYPE_CHOICES,
         default='writer',
     )
@@ -16,29 +17,81 @@ class User(AbstractUser):
         return str(self.user_type)
 
 
+class Theme(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Writer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='writer_profile')
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return str(self.name)
 
 
 class Editor(models.Model):
+    AVAILABILITY_TYPES = [('occupied', 'Ocupat'), ('available', 'Disponible')]
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='editor_profile')
-    name = models.CharField(max_length=50)
+    assigned_theme = models.ForeignKey(Theme, null=True, on_delete=models.CASCADE)
+    availability = models.CharField(null=True, max_length=255, choices=AVAILABILITY_TYPES)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return str(self.name)
+
 
 class MainEditor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='main_editor_profile')
-    name = models.CharField(max_length=50)
+    assigned_theme = models.ForeignKey(Theme, null=True, on_delete=models.CASCADE)
+    availability = models.CharField(null=True, max_length=255, choices=Editor.AVAILABILITY_TYPES)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return str(self.name)
 
 
+# TODO revisar nulls
 class Book(models.Model):
-    title = models.CharField(max_length=50)
+    BOOK_STATUSES = [('presented', 'Presentat'), ('revised', 'Revisat'), ('modifying', 'Modificant'),
+                     ('accepted', 'Aceptat'), ('rejected', 'Rebutjat'), ('published', 'Publicat')]
+    title = models.CharField(null=False, max_length=255)
     author = models.ForeignKey(Writer, null=True, on_delete=models.PROTECT)
+    assigned_to = models.ForeignKey(Editor, null=True, on_delete=models.PROTECT, related_name='books_assigned')
+    book_status = models.CharField(null=True, max_length=255, choices=BOOK_STATUSES)
+    description = models.TextField(null=True)
+    theme = models.ForeignKey(Theme, null=True, on_delete=models.PROTECT, related_name='books_themes')
+    path = models.FileField(null=True)
+    main_editor_comment = models.TextField(null=True)
+    #latest_book = models.ForeignKey("self", null=True, on_delete=models.CASCADE, related_name='old_versions')#TODO test
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Notification(models.Model):
+    NOTIFICATION_CHOICES = [('modification', 'Modificacio'), ('message', 'Missatge'), ('presented', 'Presentat'),
+                            ('assigned', 'Assignat')]
+    content = models.CharField(max_length=255)
+    notification_type = models.CharField(max_length=255, choices=NOTIFICATION_CHOICES)
+
+    def __str__(self):
+        return str(self.notification_type)
+
+
+class NotificationTable(models.Model):
+    destination_user = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='user_notifications')
+    date_received = models.DateField()
+    notification = models.ForeignKey(Notification, null=True, on_delete=models.PROTECT, related_name='type_of_notification')
+
+    def __str__(self):
+        return "Notification " + str(self.id) + "for: " + str(self.destination_user)
+
+
+class Message(models.Model):
+    destination_user = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='user_messages')
+    book = models.ForeignKey(Book, null=True, on_delete=models.PROTECT, related_name='book_messages')
+    content = models.CharField(max_length=255)
+    date_received = models.DateField()
